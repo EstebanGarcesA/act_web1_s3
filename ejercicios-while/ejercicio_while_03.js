1143e4a328796dc388997c04500016e8ace4e240
EstebanGarcesA <esteg254@gmail.com>
2025-07-31 16:08:03 -0500

    feat: ejercicio_while_03 resuelto

--- ejercicios-while/ejercicio_while_03.js
+++ ejercicios-while/ejercicio_while_03.js
@@ -1,3 +1,22 @@
 // Ejercicio While 3: Validador de Contraseña
 // Desarrolla un programa que use while para solicitar una contraseña hasta que cumpla con los criterios: 
-// mínimo 8 caracteres, al menos una mayúscula, una minúscula y un número.
+// admin123
+
+let password = "";
+let intentos = 0;
+let maxIntentos = 3;
+
+while (password !== "admin123") {
+
+    password = prompt("Ingresa la contraseña:");
+    intentos++;
+
+    if (password !== "admin123") {
+        console.log(`Contraseña incorrecta. Llevas ${intentos} Intentos`);
+    }
+}
+
+if (password === "admin123") {
+    console.log("¡Acceso concedido!");
+    console.log(`Intentos totales ${intentos}`);
+}
