ace0d8dca127b9e94f6b709fb078f4ff56c810a9
EstebanGarcesA <esteg254@gmail.com>
2025-08-07 12:49:03 -0500

    feat: ejercicio 10 for resuelto

--- ejercicios-for/ejercicio_for_10.js
+++ ejercicios-for/ejercicio_for_10.js
@@ -1,6 +1,25 @@
-let palabra = "tacocat";
-let palindromo = false;
 
-for(let i = 0;i<palabra.length;i++){
+let texto = "taco cat";
+let textoInvertida = "";
+let textoLimpio ="";
 
+for(let i=0;i<texto.length;i++){
+    let caracter = texto[i];
+    if(caracter !== " "){
+        textoLimpio += caracter.toLowerCase();
+    }
 }
+
+let textoLimpioInvertido = "";
+
+for (let i = textoLimpio.length - 1; i >= 0; i--) {
+    textoLimpioInvertido += textoLimpio[i];
+}
+
+
+if (textoLimpio === textoLimpioInvertido) {
+    console.log(`El texto ${texto} es un palindromo`);
+} else {
+    console.log(`El texto ${texto} no es un palindromo`);
+}
+
