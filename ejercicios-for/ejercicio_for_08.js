91dc9989f1819c83ae03acc5ce8d455ad3317c63
EstebanGarcesA <esteg254@gmail.com>
2025-08-06 20:53:00 -0500

    feat: ejercicio 7-9 for resuelto

--- ejercicios-for/ejercicio_for_08.js
+++ ejercicios-for/ejercicio_for_08.js
@@ -1,3 +1,27 @@
-// Ejercicio For 8: Contador de Caracteres
-// Desarrolla un programa que use un ciclo for para contar la frecuencia de cada carácter en una cadena de texto. 
-// Muestra los resultados ordenados alfabéticamente.
+let frase = "Esta es una frase";
+
+frase=frase.toLowerCase();
+
+let conteoA=0;
+let conteoE=0;
+let conteoI=0;
+let conteoO=0;
+let conteoU=0;
+
+for(let i = 0; i < frase.length; i++){
+let letra = frase[i];
+if (letra==="a") {
+    conteoA++
+} else if (letra==="e") {
+    conteoE++
+}else if (letra==="i") {
+    conteoI++
+}else if (letra==="o") {
+    conteoO++
+} else if(letra==="u"){
+    conteoU++
+}
+    
+}
+
+console.log("Cantidad de vocales en la frase: " + frase + "\nCantidad de A: " + conteoA + "\nCantidad de E: " + conteoE + "\nCantidad de I: " + conteoI + "\nCantidad de O: " + conteoO + "\nCantidad de U: " + conteoU);
--- ejercicios-for/ejercicio_for_09.js
+++ ejercicios-for/ejercicio_for_09.js
@@ -1,3 +1,11 @@
-// Ejercicio For 9: Simulador de Dados
-// Crea un simulador que use un ciclo for para lanzar dos dados 1000 veces. 
-// Calcula y muestra la frecuencia de cada suma posible (2-12) y determina cuál es la más común.
+
+let suma = 2;
+let sumaTotal= 0;
+
+for(let i=0;i<10;i++){
+console.log(suma);
+suma += suma
+sumaTotal +=suma
+}
+
+console.log(`La suma total de la secuencia es: ${sumaTotal-suma} `);
